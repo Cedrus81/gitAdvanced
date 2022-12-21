b7b685dd488875687584d2ae746d1293d1aabd70
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-21 15:08:08 +0200

    made the neccesary cmps, mvp on add-msg

---
+++ src/cmps/msg-list.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="msg-list">
+        <li class="msg-container" v-for="msg in msgs">
+            user: {{ msg.txt }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default{
+    data(){
+        demoMsgs: [
+            {
+                user: 'Erez',
+                txt: 'blabla'
+            },
+            {
+                user: 'Nir',
+                txt: 'blabla'
+            },
+            {
+                user: 'Miriam',
+                txt: 'blabla'
+            },
+            {
+                user: 'Avishai',
+                txt: 'blabla'
+            },
+        ]
+    }
+}
+</script>
