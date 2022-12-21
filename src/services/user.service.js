e75a86387774307c0d190998fca118ca39f9b63f
Miriam Smith <miriamzeff@gmail.com>
2022-12-21 15:41:26 +0200

    message and user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,101 +1,51 @@
 import { storageService } from './async-storage.service'
 // import { httpService } from './http.service'
-import { store } from '../store/store'
-import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from './socket.service'
-import { showSuccessMsg } from './event-bus.service'
 
-const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
+const STORAGE_KEY = 'users'
 
 export const userService = {
-    login,
-    logout,
-    signup,
-    getLoggedinUser,
-    saveLocalUser,
-    getUsers,
+    query,
     getById,
+    save,
     remove,
-    update,
-    changeScore
+    getEmptyUser,
 }
+window.cs = userService
 
-window.userService = userService
-
-
-function getUsers() {
-    return storageService.query('user')
-    // return httpService.get(`user`)
-}
-
-function onUserUpdate(user) {
-    showSuccessMsg(`This user ${user.fullname} just got updated from socket, new score: ${user.score}`)
-    store.dispatch({ type: 'setWatchedUser', user })
+async function query() {
+    var users = await storageService.query(STORAGE_KEY)
+    return users
+    // return httpService.get(STORAGE_KEY, filterBy)
 }
 
 async function getById(userId) {
-    const user = await storageService.get('user', userId)
-    // const user = await httpService.get(`user/${userId}`)
-
-    // socketService.emit(SOCKET_EMIT_USER_WATCH, userId)
-    socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
-    socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
-
-    return user
+    return storageService.get(STORAGE_KEY, userId)
+    // return httpService.get(`user/${userId}`)
 }
 function remove(userId) {
     return storageService.remove('user', userId)
     // return httpService.delete(`user/${userId}`)
 }
 
-async function update(user) {
-    await storageService.put('user', user)
-    // user = await httpService.put(`user/${user._id}`, user)
-    // Handle case in which admin updates other user's details
-    if (getLoggedinUser()._id === user._id) saveLocalUser(user)
-    return user
+async function save(user) {
+    var savedUser
+    if (user._id) {
+        // saveduser = await storageService.put(STORAGE_KEY, user)
+        savedUser = await httpService.put(`user/${user._id}`, user)
+    } else {
+        // saveduser = await storageService.post(STORAGE_KEY, user)
+        savedUser = await httpService.post('user', user)
+    }
+    return savedUser
 }
 
-async function login(userCred) {
-    const users = await storageService.query('user')
-    const user = users.find(user => user.username === userCred.username)
-    // const user = await httpService.post('auth/login', userCred)
-    if (user) {
-        // socketService.login(user._id)
-        return saveLocalUser(user)
+function getEmptyUser() {
+    return {
+        email: '',
+        imgUrl: ''
     }
 }
-async function signup(userCred) {
-    userCred.score = 10000
-    if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
-    const user = await storageService.post('user', userCred)
-    // const user = await httpService.post('auth/signup', userCred)
-    // socketService.login(user._id)
-    return saveLocalUser(user)
-}
-async function logout() {
-    sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
-    socketService.logout()
-    // return await httpService.post('auth/logout')
-}
 
-async function changeScore(by) {
-    const user = getLoggedinUser()
-    if (!user) throw new Error('Not loggedin')
-    user.score = user.score + by || by
-    await update(user)
-    return user.score
-}
-
-
-function saveLocalUser(user) {
-    user = {_id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, score: user.score}
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
-    return user
-}
-
-function getLoggedinUser() {
-    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
-}
 
 
 // ;(async ()=>{
